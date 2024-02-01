30ffb7465062c0574321205d11fca3d06cba9d1b
Priyanshu Dey <omipriyanshu10@gmail.com>
2024-02-01 13:31:52 +0530

    Added Context API

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 
+import { useState } from 'react'
 import './App.css'
+import AddTodo from './components/AddTodo/AddTodo'
+import TodoList from './components/TodoList/TodoList'
+import TodoContext from './context/TodoContext'
 
 function App() {
 
+  const [todos,setTodos] = useState([
+    {id: 1, text: 'todo 1', isFinished: true},
+    {id: 2, text: 'todo 2', isFinished: false},
+
+  ]);
+
+  
   return (
     <>
-      ToDos
+      <TodoContext.Provider value={{todos, setTodos}}>
+        <AddTodo />
+        <TodoList />   
+      </TodoContext.Provider>
     </>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/AddTodo/AddTodo.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable react/prop-types */
+import { useContext, useState } from "react";
+import TodoContext from "../../context/TodoContext";
+
+function AddTodo(){
+    const {todos, setTodos} = useContext(TodoContext)
+    const [todoText,setTodoText] = useState('');
+    return(
+        <>
+            <input 
+                placeholder="Add your next todo...."
+                onChange={(e)=> setTodoText(e.target.value)}
+                value={todoText}
+            />
+            <button onClick={()=>{
+                let nextId = todos.length + 1;
+                setTodos([...todos, {id : nextId, text : todoText, isFinished : false}]);
+                setTodoText('');
+            }}>Submit</button>
+        </>
+    )
+}
+
+export default AddTodo;
